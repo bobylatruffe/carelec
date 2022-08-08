85f16ff0e446e5c05e35365be95c159b2805bd2a
Bozlak Fatih <bozlak.fatih@gmail.com>
2022-08-08 13:22:16 +0200

    CCM :   - mise en place du content à gauche, à droite ;   - reste à mettre la barre vertical et le logo de la voiture ;

--- app/src/Component/Ccm/Ccm.jsx
+++ app/src/Component/Ccm/Ccm.jsx
@@ -12,22 +12,22 @@ class Ccm extends React.Component {
           <p>Vous trouverez la description des différentes étapes afin de mieux comprendre comment nous travaillons, mais surtout comment notre service fonctionne.</p>
         </div>
 
-        <div className="ccm-content-n">
-          <h3>Commencer par <span className="red">estimer</span> le coût de l'entretien de votre véhicule.</h3>
+        <div className="ccm-content-n ccm-left">
+          <h3>Commencer par <span className="red">estimer</span> <br />le coût de l'entretien de votre véhicule.</h3>
           <p>Saisissez simplement votre plaque d'immatriculation, et nous identifierons le modèle exact de votre véhicule, afin de vous fournir un coût le plus juste.</p>
         </div>
 
-        <div className="ccm-content-n">
-          <h3>Choisissez la date et un créneau pour qu'on puisse venir <span class="red">récupérer</span> votre véhicule.</h3>
+        <div className="ccm-content-n ccm-right">
+          <h3>Choisissez la date et un créneau pour <br />qu'on puisse venir <span class="red">récupérer</span> votre véhicule.</h3>
           <p>Après avoir pris connaissance du coût de votre entretien, choisissez une date et un créneau horaire afin qu'un de nos mécaniciens vienne récupérer votre véhicule pour réaliser son entretien. Il réalisera également un état des lieux sur votre véhicule.</p>
         </div>
 
-        <div className="ccm-content-n">
-          <h3><span class="red">Suivez en direct</span> tout ce qu'il se passe avec votre véhicule.</h3>
+        <div className="ccm-content-n ccm-left">
+          <h3><span class="red">Suivez en direct</span><br /> tout ce qu'il se passe avec votre véhicule.</h3>
           <p>A partir du moment ou vous avez pris un rendez-vous, depuis votre espace personnel vous pouvez suivre l'avancement des différentes étapes (recherche de votre véhicule, pièces changées, opération en cours ...) qui sont réalisées.</p>
         </div>
-           
-        <div className="ccm-content-n">
+
+        <div className="ccm-content-n ccm-right">
           <h3><span class="red">Restitution</span> de votre véhicule et paiement.</h3>
           <p>Une fois que l'entretien de votre véhicule est terminé, notre mécanicien vous rapporte votre véhicule, un état des lieux est effectué et comparé avec la 1er. Si tout vous semble correct, alors vous pourrez réaliser le paiement de la prestation directement à notre mécanicien.</p>
         </div>
