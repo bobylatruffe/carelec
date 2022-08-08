1323289a7b15e1c087fb8f439a08d71411b0b9eb
Bozlak Fatih <bozlak.fatih@gmail.com>
2022-08-08 18:43:51 +0200

    Possibilter de saisir sa plaque d'immatriculation :   - centrer dans une div id=estimer ;     - faut veiller à bien utiliser ce div pour ne pas casser le composant GetItemArrow de la page d'acueil ;

--- app/src/Component/Estimer/Estimer.jsx
+++ app/src/Component/Estimer/Estimer.jsx
@@ -7,7 +7,13 @@ import GetImmatArrow from '../GetImmatArrow/GetImmatArrow';
 class Estimer extends React.Component {
   render() {
     return (
-      <GetImmatArrow  />
+      <div id="estimer">
+        <GetImmatArrow />
+        <div id="description">
+          <h1><span class="red">Estimer</span> le cout de votre entretien</h1>
+          <p>Nous avons beosin de votre plaque d'immatriculation afin d'identifier le modèle de votre véhicule.</p>
+        </div>
+      </div>
     )
   }
 }
