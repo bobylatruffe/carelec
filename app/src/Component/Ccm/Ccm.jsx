7b4456a4bba0fdeeaf48517ef93f1d0986efbde6
Stykame <marc.lozachmeur@hotmail.fr>
2022-08-09 09:02:41 +0200

    Mise à jour des textes du site

--- app/src/Component/Ccm/Ccm.jsx
+++ app/src/Component/Ccm/Ccm.jsx
@@ -9,27 +9,27 @@ class Ccm extends React.Component {
 
         <div className="ccm-content-n">
           <h1><span className="red">Comment</span> ça marche ?</h1>
-          <p>Vous trouverez la description des différentes étapes afin de mieux comprendre comment nous travaillons, mais surtout comment notre service fonctionne.</p>
+          <p>Vous trouverez ci-dessous la description des différentes étapes afin de mieux comprendre comment nous travaillons, mais surtout comment notre service fonctionne.</p>
         </div>
 
         <div className="ccm-content-n ccm-left">
-          <h3>Commencer par <span className="red">estimer</span> <br />le coût de l'entretien de votre véhicule.</h3>
-          <p>Saisissez simplement votre plaque d'immatriculation, et nous identifierons le modèle exact de votre véhicule, afin de vous fournir un coût le plus juste.</p>
+          <h3><span className="red">Estimer</span> le coût d'entretien de votre véhicule.</h3>
+          <p>Saisissez simplement votre plaque d'immatriculation et nous identifierons le modèle exact de votre véhicule. Ainsi nous vous proposerons des équipements adaptés au coût le plus juste.</p>
         </div>
 
         <div className="ccm-content-n ccm-right">
-          <h3>Choisissez la date et un créneau pour <br />qu'on puisse venir <span class="red">récupérer</span> votre véhicule.</h3>
-          <p>Après avoir pris connaissance du coût de votre entretien, choisissez une date et un créneau horaire afin qu'un de nos mécaniciens vienne récupérer votre véhicule pour réaliser son entretien. Il réalisera également un état des lieux sur votre véhicule.</p>
+          <h3><span class="red">Prise en charge</span> de votre véhicule.<br />Choisissez la date et l'heure parmis nos créneaux disponibles et nous venons chercher votre véhicule au lieu souhaité.</h3>
+          <p>Après avoir pris connaissance du coût de votre entretien, choisissez le moment et l'endroit où notre mécanicien viendra récupérer votre véhicule afin de réaliser la prestation. Dans un soucis de transparence, il réalisera à la prise en main un état des lieux de votre véhicule avec photos.</p>
         </div>
 
         <div className="ccm-content-n ccm-left">
-          <h3><span class="red">Suivez en direct</span><br /> tout ce qu'il se passe avec votre véhicule.</h3>
-          <p>A partir du moment ou vous avez pris un rendez-vous, depuis votre espace personnel vous pouvez suivre l'avancement des différentes étapes (recherche de votre véhicule, pièces changées, opération en cours ...) qui sont réalisées.</p>
+          <h3><span class="red">Suivez en direct</span> tout ce qu'il se passe avec votre véhicule.</h3>
+          <p>Dès la prise en main de votre véhicule par notre équipe, vous pouvez depuis votre espace personnel sur notre site suivre l'avancement des différentes étapes réalisées sur votre véhicule (sa localisation, les pièces changées, les opérations en cours, etc.).</p>
         </div>
 
         <div className="ccm-content-n ccm-right">
           <h3><span class="red">Restitution</span> de votre véhicule et paiement.</h3>
-          <p>Une fois que l'entretien de votre véhicule est terminé, notre mécanicien vous rapporte votre véhicule, un état des lieux est effectué et comparé avec la 1er. Si tout vous semble correct, alors vous pourrez réaliser le paiement de la prestation directement à notre mécanicien.</p>
+          <p>Une fois l'entretien de votre véhicule terminé, notre mécanicien vous rapporte votre véhicule au lieu convenu. Un nouvel état des lieux est effectué et comparé avec le premier. Le paiement de la prestation se fait directement sur place auprès de notre mécanicien. Vous pouvez alors prendre la route en toute sécurité.</p>
         </div>
 
         <div id="line-vertical"></div>
