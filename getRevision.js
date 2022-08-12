9c03cc34345823628a744f65cb0add133505d574
Fatih Bozlak <bozlak.fatih@gmail.com>
2022-08-12 10:29:38 +0200

    ajout des méta data getRevision.js

--- getRevision.js
+++ getRevision.js
@@ -1,7 +1,21 @@
+/**
+ * Nom ...................... : getRevision.js
+ * Rôle ..................... : 
+ * 
+C'est un "wraper" de makeMarqueModelCv.js et searchModelInFileFs.js utilisant une API pour récupérer les informatins d'un véhicule depuis sa plaque d'immatriculation.
+Ce module-wraper, permet de lister toutes les préconisations constructeur d'un modèle de véhicule.
+
+ * Autheur .................. : Bozlak Fatih 1503001522G
+ * Licence .................. : IED L2 Informatique
+ * Année .................... : 2021/2022
+ * Usage .................... : node getRevision.js AANNNAA 
+ **/
+
 const searchModelInFileFs = require('./searchModelInFileFs.js');
 const makeMarqueModelCv = require('./makeMarqueModelCv');
 
 const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
 
 const apiRaw = fs.readFileSync("./api-plaque.txt");
 const apiPlaque = apiRaw.toString();
@@ -16,6 +30,8 @@ fetch(apiPlaque + process.argv[2])
 
     let standardise = makeMarqueModelCv(libelle);
     console.log(standardise);
-    
-    console.log(searchModelInFileFs(standardise));
+
+    let revisions = searchModelInFileFs(standardise);
+
+    return revision;
   })
